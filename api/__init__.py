ed5b61dc68ce2840c94dfb7bc9a61f8fb16d89b2
Emmanuel <femiemmanuel1990@gmail.com>
2023-03-14 20:07:05 +0100

    Created function to initialize main app

--- api/__init__.py
+++ api/__init__.py
@@ -8,23 +8,35 @@ import config
 import pandas as pd
 
 
-# Create and Configure Flask App
-app = Flask(__name__)
-CONFIG_TYPE = os.getenv('CONFIG_TYPE', default=config.DevelopmentConfig)
-app.config.from_object(CONFIG_TYPE)
+def create_api():
 
-# Create and Connect to database
-db = declarative_base()
+    # Create and Configure Flask App
+    app = Flask(__name__)
+    CONFIG_TYPE = os.getenv('CONFIG_TYPE', default=config.DevelopmentConfig)
+    app.config.from_object(CONFIG_TYPE)
 
-# Create a connection to the duckdb Engine
-eng = create_engine("duckdb://")
-db.metadata.create_all(eng)
+    return app
 
-# Create Session Object for queries
-session = Session(bind=eng)
 
-# write the DataFrame to the DuckDB databasethon
-user_df.to_sql('User', eng, if_exists='replace', index=False)
-user_events_df.to_sql('UserEvents', eng, if_exists='replace', index=False)
+def initialize_db():
+    # Create and Connect to database
+    db = declarative_base()
+
+    # Create a connection to the duckdb Engine
+    eng = create_engine("duckdb://")
+    db.metadata.create_all(eng)
+
+    # Create Session Object for queries
+    session = Session(bind=eng)
+
+    # write the DataFrame to the DuckDB databasethon
+    user_df.to_sql('User', eng, if_exists='replace', index=False)
+    user_events_df.to_sql('UserEvents', eng, if_exists='replace', index=False)
+
+    return session, db
+
+
+app = create_api()
+session, db = initialize_db()
 
 from api import views
