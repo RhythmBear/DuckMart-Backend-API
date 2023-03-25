bf8cdf1f9336dea972b75b36096e43627447a0a8
Emmanuel <femiemmanuel1990@gmail.com>
2023-03-25 19:46:41 +0100

    Completed all Segmentation Criteria and reformated a part of the Segmentation Function.

--- api/tasks.py
+++ api/tasks.py
@@ -5,6 +5,7 @@ con = duckdb.connect(database=':memory:', read_only=False)
 
 
 def validate_date(raw_date):
+    print(raw_date)
     try:
         start_date_obj = datetime.strptime(raw_date, '%Y-%m-%d')
     except ValueError:
@@ -37,33 +38,58 @@ def load_data_from_db():
         """
         CREATE TABLE IF NOT EXISTS UserEvents(id INTEGER,
                 user_id INTEGER,
-                event_type VARCHAR,
+                event_name VARCHAR,
                 event_date DATE, 
     ); 
-        COPY UserEvents FROM 'datasets/Duckmart-user-events.csv' (DELIMITER ',', HEADER );
+        COPY UserEvents FROM 'datasets/Duckmart-User-Events.csv' (DELIMITER ',', HEADER );
     
     """)
 
     return con
 
 
-def segment_user(json_segment: dict):
+def segment_user(user_json_segment: dict):
+
     json_payload = {
-        "age": 'all',
+        "age": {'min': 0,
+                'max': 1000
+                },
         "gender": 'all',
         "location": 'all',
-        "sign_up_date": {
-            "start_date": "",
-            "end_date": ""
-        },
+        "sign_up_date": {"start_date": validate_date('1953-1-1'),
+                         "end_date": datetime.now().date()
+                         },
         "subscription_plan": 'all',
         "device_type": 'all',
-        "events": 'all'
+        "events": {'type': {'min': 0, 'max': 10}}
 
     }
 
-    for key, value in json_segment.items():
-        json_payload[key] = value
+    # Here I'm checking to make sure that the format of the json provided matches the required format i.e the
+    # keys should all be similar
+    for key, value in user_json_segment.items():
+        if key not in json_payload.keys():
+            return {"error": "Invalid key",
+                    "status": 400,
+                    "message": f"{key} is not a valid key. Please select from the list of valid keys",
+                    "valid_keys": list(json_payload.keys())}
+
+        # This line of code might look a bit confusing but here is what is happening, we are looping through the values
+        # in the provided json_segment that are dictionaries and changing the provided values to fit our json segment.
+        if type(value) == dict:
+
+            # If i do not add this if statement to check whenever the user sends an events dictionary, the code will add
+            # it to the existing dictionary instead of replacing the default
+            if key == "events":
+                json_payload[key] = value
+            else:
+                for key_i, value_i in value.items():
+                    json_payload[key][key_i] = value_i
+
+        else:
+            json_payload[key] = value
+
+    print(json_payload)
 
     user_query = con.execute("SELECT * FROM users;").df()
 
@@ -78,8 +104,17 @@ def segment_user(json_segment: dict):
         if key in ["gender", "location", "device_type", "subscription_plan"]:
             # Get valid values
             valid_values = list(con.execute(f"SELECT DISTINCT({key}) FROM users; ").fetchdf()[key])
-            if value in valid_values:
-                user_query = con.execute(f"SELECT * from previous_query WHERE {key}='{value}'").df()
+            if value in valid_values or set(value).issubset(valid_values):
+                if isinstance(value, str):
+                    user_query = con.execute(f"SELECT * from previous_query WHERE {key}='{value}'").df()
+                elif type(value) == list:
+                    print("list here")
+                    user_query = con.execute(f"""SELECT * from previous_query WHERE {key} IN {tuple(value)}""").df()
+                else:
+                    return {"error": "Invalid DataType",
+                            "status": 400,
+                            "message": f"Invalid Data type, Please pass in a String or a List of Strings {key}"}
+
             else:
                 return {"error": "Invalid value",
                         "status": 400,
@@ -90,28 +125,75 @@ def segment_user(json_segment: dict):
             # Check to make sure that a dictionary is provided
             if type(value) is dict:
 
-                try:
-                    start_date = validate_date(value["start_date"])
-
-                except KeyError:
-                    start_date = validate_date('1753-1-1')
-
-                try:
-                    end_date = validate_date(value["end_date"])
-
-                except KeyError:
-                    end_date = datetime.now().date()
-
-                # if Both start and end dates are provided:
-                if start_date is not None and end_date is not None:
+                if value['start_date'] is not None and value['end_date'] is not None:
                     user_query = con.execute(f"""SELECT * 
                                              FROM previous_query 
-                                             WHERE sign_up_date BETWEEN '{start_date}' AND '{end_date}'""").df()
+                                             WHERE sign_up_date BETWEEN '{value['start_date']}' AND '{value['end_date']}'""").df()
+
+            else:
+                return {'message': "Invalid DataType, please make sure you enter a dictionary",
+                        "status": 400,
+
+                }
 
         elif key == "age":
-            pass
+            if type(value) == dict:
+                user_query = con.execute(f"""
+                                         SELECT * 
+                                         FROM previous_query 
+                                         WHERE age BETWEEN '{value["min"]}' AND '{value["max"]}'""").df()
 
-    return user_query
+            else:
+                return {'message': "Invalid DataType, please make sure you enter a Dictionary",
+                        'status': 400}
+
+        elif key == "events":
+            if 'type' in value.keys():
+                continue
+
+            event_user_ids = []
+
+            for event_key, event_value in value.items():
+                try:
+                    min_val = event_value["min"]
+                except KeyError as err:
+                    min_val = 0
+
+                try:
+                    max_val = event_value["max"]
+                except KeyError as err:
+                    max_val = 1000
+
+                # print(event_key, event_value)
+                # print("I got here")
+                user_ids = con.execute(f"""
+                                    SELECT user_id, count(*) As event_count
+                                    FROM UserEvents
+                                    WHERE event_name='{event_key}'
+                                    GROUP BY user_id
+                                    HAVING event_count BETWEEN {min_val} AND {max_val}
+                                    ORDER BY user_id;
+                                    """).df()
+                print(user_ids)
+                event_user_ids.append(list(user_ids['user_id']))
+
+            print(event_user_ids)
+            # Get all the User ID's that appear in all the lists
+            common_ids = set(event_user_ids[0])
+            for sublist in event_user_ids:
+                common_ids = common_ids.intersection(set(sublist))
+
+            user_query = con.execute(f"""SELECT p.ID 
+                                                 FROM previous_query AS p 
+                                                 WHERE p.ID IN {tuple(common_ids)} """).df()
+        else:
+            return {'message': "Invalid DataType, please make sure you enter a Dictionary",
+                    'status': 400}
+
+    result = {'message': "Success",
+              'status': 200,
+              'data': list(user_query['id'])}
+    return result
 
 
 con = load_data_from_db()
--- api/views.py
+++ api/views.py
@@ -1,54 +1,89 @@
 import requests
 from flask import jsonify, request
 from main import app
-from api.tasks import con
+from api.tasks import con, segment_user
 
 
 @app.route('/', methods=['GET'])
 def home():
     # Read all the data from the database
-    return "Welcome to my API. Please refer to the Documentation to learn more."
+    return """<html>
+  <head>
+    <title>Welcome to my API</title>
+  </head>
+  <body>
+    <h1>Welcome to my API</h1>
+    <p>Please refer to the <a href="https://documenter.getpostman.com/view/21203353/2s93RNyaTX">documentation</a> for more information.</p>
+  </body>
+</html>"""
 
 
-@app.route('/v1/users', methods=['GET'])
+@app.route('/v1/users/', methods=['GET'])
 def get_users_data():
-    all_users = con.execute("""
-    SELECT * FROM users
-    """).fetchdf()
+    user_id = request.json['user_id']
+    print(user_id)
+    print(type(user_id))
 
-    result = [data._asdict() for data in all_users.itertuples(index=False)]
+    if user_id == '':
+        all_users = con.execute("""
+        SELECT * FROM users
+        """).fetchdf()
 
-    return jsonify(result)
+    elif isinstance(user_id, list):
+        all_users = con.execute(f"""
+        SELECT * FROM users 
+        WHERE id IN {tuple(user_id)};""").df()
+    elif isinstance(user_id, int):
+        all_users = con.execute(f"""
+        SELECT * FROM users 
+        WHERE id={user_id};""").fetchdf()
+    else:
+        return {'status': 400,
+               'message': 'Type Error. user_id must be an integer or a list of integers'}
+
+    result = {'status': 200,
+              "data": [data._asdict() for data in all_users.itertuples(index=False)]
+              }
+
+    return jsonify(result), 200
 
 
-@app.route('/v1/user/<id>', methods=['GET'])
-def get_user(id):
-    user = con.execute(f"""
-        SELECT * 
-        FROM users 
-        WHERE ID={id};
-    """).fetchdf()
-
-    user_data = user.to_dict(orient='records')
-
-    return jsonify(user_data)
-
-
-@app.route('/v1/userevents/', methods=['GET'])
+@app.route('/v1/UserEvents/', methods=['GET'])
 def get_user_events():
-    args = request.args.to_dict()
 
-    for key, value in args.items():
-        print(f"{key} : {value}")
+    user_id = request.json['user_id']
+    # print(user_id)
+    if isinstance(user_id, list):
+        user = con.execute(f"""
+            SELECT * 
+            FROM UserEvents 
+            WHERE user_id IN {tuple(user_id)};
+        """).fetchdf()
+
+    elif isinstance(user_id, int):
+        user = con.execute(f"""
+            SELECT * 
+            FROM UserEvents 
+            WHERE user_id={user_id};
+        """).fetchdf()
+
+    else:
+        return {'status': 400,
+                'message': 'Type Error. user_id must be an integer or a list of integers'}, 400
+
+    result = {'status': 200,
+              'data': [data._asdict() for data in user.itertuples(index=False)]}
+
+    return jsonify(result), 200
+
+
+@app.route('/v1/Fetch-Users/', methods=['GET'])
+def fetch_users():
 
     # Check to make sure that the values are columns in the database
 
-    user_events = con.execute(f"""
-        SELECT * 
-        FROM UserEvents 
-        ORDER BY event_date ASC;
-    """).fetchdf()
+    print(request.json)
 
-    user_events_data = user_events.to_dict(orient='records')
+    result = segment_user(request.json)
 
-    return jsonify(user_events_data)
+    return jsonify(result)
